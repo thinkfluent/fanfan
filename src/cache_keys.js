524bcf007f75012ae186d9b4779d45018895d16d
tomwalder <tom@thinkfluent.co.uk>
2023-07-21 15:18:41 +0100

    Fix race condition for JobDone, avoiding duplicate JobDone message publication

--- src/cache_keys.js
+++ src/cache_keys.js
@@ -10,5 +10,6 @@ module.exports.coreKeys = (jobId) => {
         `fanfan-${jobId}-start`,
         `fanfan-${jobId}-tasks`,
         `fanfan-${jobId}-request`,
+        `fanfan-${jobId}-done`,
     ];
 }
--- src/controllers/task_done.js
+++ src/controllers/task_done.js
@@ -24,7 +24,7 @@ module.exports = (req, res) => {
 
     // Redis keys
     const [taskOkCountKey, taskFailCountKey] = cacheKeys.countKeys(jobId);
-    const [jobStartKey, jobTasksKey, jobRequestKey] = cacheKeys.coreKeys(jobId);
+    const [jobStartKey, jobTasksKey, jobRequestKey, jobDoneKey] = cacheKeys.coreKeys(jobId);
 
     const thisTaskFailed = (itemStatus.task.SUCCEEDED !== taskOutcome.status);
     const incrTargetKey = thisTaskFailed ? taskFailCountKey : taskOkCountKey;
@@ -35,39 +35,50 @@ module.exports = (req, res) => {
                 // Tasks remaining: sCardResult
                 if (sCardResult < 1) {
                     // All tasks done
-                    redisClient.mGet([taskOkCountKey, taskFailCountKey, jobStartKey, jobRequestKey]).then((mGetResult) => {
-                        // Cleanup (delete counts, job meta, task list)
-                        redisClient.del([taskOkCountKey, taskFailCountKey, jobStartKey, jobTasksKey, jobRequestKey]);
-                        // Prep stats payload
-                        const [taskOkCount, taskFailCount, jobStartTime, jobRequestJson] = mGetResult;
-                        const jobSuccessful = (!thisTaskFailed && !taskFailCount);
-                        const jobSummary = {
-                            'jobId': jobId,
-                            'request': JSON.parse(jobRequestJson),
-                            'status': jobSuccessful ? itemStatus.job.SUCCEEDED : itemStatus.job.FAILED,
-                            'taskCounts': {
-                                [itemStatus.task.SUCCEEDED]: parseInt(taskOkCount || 0),
-                                [itemStatus.task.FAILED]: parseInt(taskFailCount || 0),
-                            },
-                            'startedTsp': Math.ceil(jobStartTime / 1000),
-                            'tookMs': Date.now() - jobStartTime
-                        };
-                        logger.info(jobSummary, 'Job (all tasks) complete');
+                    // Check for race condition on last Task / Job completion
+                    redisClient.setNX(jobDoneKey, jobDoneKey).then((setNXResult) => {
+                        if (setNXResult) {
+                            // We have the "lock" for the JobDone check
+                            redisClient.mGet([taskOkCountKey, taskFailCountKey, jobStartKey, jobRequestKey]).then((mGetResult) => {
+                                // Cleanup (delete counts, job meta, task list)
+                                redisClient.del([taskOkCountKey, taskFailCountKey, jobStartKey, jobTasksKey, jobRequestKey, jobDoneKey]);
+                                // Prep stats payload
+                                const [taskOkCount, taskFailCount, jobStartTime, jobRequestJson] = mGetResult;
+                                const jobSuccessful = (!thisTaskFailed && !taskFailCount);
+                                const jobSummary = {
+                                    'jobId': jobId,
+                                    'request': JSON.parse(jobRequestJson),
+                                    'status': jobSuccessful ? itemStatus.job.SUCCEEDED : itemStatus.job.FAILED,
+                                    'taskCounts': {
+                                        [itemStatus.task.SUCCEEDED]: parseInt(taskOkCount || 0),
+                                        [itemStatus.task.FAILED]: parseInt(taskFailCount || 0),
+                                    },
+                                    'startedTsp': Math.ceil(jobStartTime / 1000),
+                                    'tookMs': Date.now() - jobStartTime
+                                };
+                                logger.info(jobSummary, 'Job (all tasks) complete');
 
-                        // Publish Job Done
-                        pubsub.getTopic(appConfig.pubsub_topic_job_done).publishMessage({
-                            data: Buffer.from(JSON.stringify(jobSummary))
-                        }).then((messageId) => {
-                            res.json({jobId, ok: jobSuccessful});
-                        }).catch((error) => {
-                            logger.error({}, 'Failed to publish fan-in message');
-                            // @todo handle error publishing final FAN-IN action (e.g. retry)
-                            // @todo store in Redis for re-publish?
-                            // @todo consider setTimeout() with republish?
-                        });
+                                // Publish Job Done
+                                pubsub.getTopic(appConfig.pubsub_topic_job_done).publishMessage({
+                                    data: Buffer.from(JSON.stringify(jobSummary))
+                                }).then((messageId) => {
+                                    res.json({jobId, ok: jobSuccessful});
+                                }).catch((error) => {
+                                    logger.error({}, 'Failed to publish fan-in message');
+                                    // @todo handle error publishing final FAN-IN action (e.g. retry)
+                                    // @todo store in Redis for re-publish?
+                                    // @todo consider setTimeout() with republish?
+                                });
+                            });
+                        } else {
+                            logger.debug(`JobDone race condition avoided. setNX result for done was ${setNXResult}`);
+                        }
+                    }).catch((error) => {
+                        // @todo determine best approach here (e.g. carry out JobDone work anyway)
+                        logger.warn(error, 'Failed to setNX for JobDone race condition avoidance');
                     });
                 } else {
-                    // @todo still tasks to go
+                    // Still tasks remaining
                     res.json({jobId});
                 }
             }).catch((error) => {
